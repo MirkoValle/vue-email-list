0f49e65261d958446e15cb7d2f25d6eb74281dfc
MirkoValle <mirko.valle20@gmail.com>
2024-06-05 18:16:06 +0200

    Added vue on html in order to print all the emails at the same time

--- js/script.js
+++ js/script.js
@@ -14,13 +14,10 @@ createApp({
             for (let index = 0; index < 10; index++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                     .then((response) => {
-                        console.log(response.data.response)
-                        this.emails.push(response.data.response)
-                    })
-
+                        this.emails.push(response.data.response);
+                    });
             }
-            console.log(this.emails)
-        }
+        },
     },
     created() {
         this.getEmail();
